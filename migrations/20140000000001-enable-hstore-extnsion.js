78a7a51021216a84175cb38231328a2df5af0259
fundon <cfddream@gmail.com>
2014-03-14 07:15:07 +0800

    enable hstore

---
+++ migrations/20140000000001-enable-hstore-extnsion.js
@@ -0,0 +1,23 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    var sequelize = migration.queryInterface.sequelize;
+    sequelize
+      .query('CREATE EXTENSION IF NOT EXISTS hstore;')
+      .complete(function (err, res) {
+        if (err) throw(err);
+        done();
+      });
+  },
+  down: function(migration, DataTypes, done) {
+    /*
+    var sequelize = migration.queryInterface.sequelize;
+    sequelize
+      .query('DROP EXTENSION IF EXISTS hstore;')
+      .complete(function (err, res) {
+        if (err) throw(err);
+        done();
+      });
+    */
+    done();
+  }
+}
