e1c0a3af6464f9696405909be7611dcdbfe5a3cd
fundon <cfddream@gmail.com>
2014-03-18 11:21:00 +0800

    add flash auth middlewares

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+/**
+ *  Module dependencies.
+ */
+
+var debug = require('debug')('bit:auth');
+
+module.exports = function *auth(next) {
+  var session = yield this.session;
+  debug('%s, %s, %j', this.url, this.sessionId, session);
+  this.user = {};
+
+  if (session.username) {
+    this.user.username = session.username;
+    // admin
+    debug('auth exists user: %j, headers: %j', this.user, this.header);
+  }
+
+  yield *next;
+};
