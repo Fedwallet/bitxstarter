eae37631ca2e640afcc2e945307bd035de5fbf18
fundon <cfddream@gmail.com>
2014-03-25 10:47:47 +0800

    finshied `sign-up` and `login`

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,39 @@
+'use strict';
 
-exports.show = function *(next) {
-  yield this.render('users/new', {
-    authenticity_token: this.csrf
-  });
+/**
+ *  Module
+ */
+
+var validator = require('validator');
+
+/**
+ *  Models
+ */
+
+var models = require('../models')();
+var User = models.User;
+
+/**
+ *  Helpers
+ */
+
+function checkEmail(email) {
+  return validator.isLength(email, 5, 255)
+    && validator.isEmail(email);
+}
+
+function checkUsername(username) {
+  return validator.isLength(username, 1, 39)
+    &&  validator.matches(username, /^[a-zA-Z0-9][a-zA-Z0-9-]{0,38}$/);
+}
+
+function checkPassword(password) {
+  return validator.isLength(password, 8);
+}
+
+
+exports['new'] = function *(next) {
+  yield this.render('users/new');
 };
 
 exports.create = function *(next) {
@@ -11,5 +42,52 @@ exports.create = function *(next) {
   var email     = body.email.trim().toLowerCase();
   var password  = body.password.trim();
 
-  this.body = body;
+  if (!username || !email || !password) {
+    this.flash = { error: '信息不完整' };
+  } else if (!checkUsername(username)) {
+    this.flash = { error: '仅可以使用[A-Za-z0-9-]' };
+  } else if (!checkEmail(email)) {
+    this.flash = { error: '邮箱地址不正确' };
+  } else if (!checkPassword(password)) {
+    this.flash = { error: '密码至少要8位' };
+  }
+
+  var query = {
+    username: username,
+    email: email
+  };
+
+  // 填写信息不正确
+  if (this.flash && this.flash.error) {
+    yield this.render('users/new', query);
+    return;
+  }
+
+  var isAlready = yield User.getUserByUserNameOrEmail(query);
+  // 已经被注册了
+  if (isAlready) {
+    this.flash = { error: '用户名或邮箱已被使用' };
+    yield this.render('users/new', query);
+    return;
+  }
+
+  try {
+    var user = yield User.saveNew(username, email, password);
+    // 注册成功
+    if (user) {
+      this.flash = { success: '欢迎加入 ' + user.username + '!' };
+      this.session.loggedIn = true;
+      this.session.userId = user.id;
+      this.session.user = user.values;
+
+      // 如何扩展 `context`?
+      var returnTo = this.session && this.session.returnTo;
+      delete this.session.returnTo;
+      this.redirect(returnTo || '/');
+      this.body = '<html><body>You are being <a href="' + this.location + '">redirected</a>.</body></html>';
+    }
+  } catch (e) {
+    // 403?
+    this.throw(403, '注册失败！');
+  }
 };
