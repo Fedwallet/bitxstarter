887517c2e558aced16aea4d65f288b3464bc5b0f
fundon <cfddream@gmail.com>
2014-03-25 10:43:52 +0800

    add sessions

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,16 +1,80 @@
+'use strict';
 
-exports.show = function *(next) {
-  yield this.render('sessions/new', {
-    authenticity_token: this.csrf
-  });
+/**
+ *  Module
+ */
+
+var validator = require('validator');
+
+/**
+ *  Models
+ */
+
+var models = require('../models')();
+var User = models.User;
+
+
+/**
+ *  Helpers
+ */
+
+function checkEmail(email) {
+  return validator.isLength(email, 5, 255)
+    && validator.isEmail(email);
+}
+
+function checkUsername(username) {
+  return validator.isLength(username, 1, 39)
+    &&  validator.matches(username, /^[a-zA-Z0-9][a-zA-Z0-9-]{0,38}$/);
+}
+
+exports['new'] = function *(next) {
+  yield this.render('sessions/new');
 };
 
 exports.create = function *(next) {
-  //var body = this.request.body;
-  console.log(this.request.body);
+  var body = this.request.body;
+  var login = body.login.trim().toLowerCase();
+  var password  = body.password.trim();
 
-  this.body = this.request.body;
+  if (!login || !password) {
+    this.flash = { error: 'incorrect username or password' };
+    yield this.render('sessions/new', { login: login });
+    return;
+  }
+
+  var query = {};
+
+  if (/@/.test(login) && checkEmail(login)) {
+    query.email = login;
+  } else if (checkUsername(login)) {
+    query.username = login;
+  } else {
+    this.flash = { error: 'incorrect username or password' };
+    yield this.render('sessions/new', { login: login });
+    return;
+  }
+
+  var user = yield User.getUserByUserNameOrEmail(query);
+
+  if (user && user.comparePassword(password)) {
+    this.flash = { success: '登录成功！' };
+    this.session.loggedIn = true;
+    this.session.userId = user.id;
+    this.session.user = user.values;
+
+    // 如何扩展 `context`?
+    var returnTo = this.session && this.session.returnTo;
+    delete this.session.returnTo;
+    this.redirect(returnTo || '/');
+    this.body = '<html><body>You are being <a href="' + this.location + '">redirected</a>.</body></html>';
+  } else {
+    this.flash = { error: 'incorrect username or password' };
+    yield this.render('sessions/new', { login: login });
+  }
 };
 
 exports.destory = function *(next) {
+  this.session = null;
+  this.redirect('/');
 };
